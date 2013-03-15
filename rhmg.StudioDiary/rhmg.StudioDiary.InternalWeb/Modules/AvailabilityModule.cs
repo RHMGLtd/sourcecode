e3845807f6e6d9db53a03b4b419ccb534dcc4943
roo <roo@rhmg.co.uk>
2013-03-15 21:28:15 +0000

    fixed the current day indicator

--- rhmg.StudioDiary/rhmg.StudioDiary.InternalWeb/Modules/AvailabilityModule.cs
+++ rhmg.StudioDiary/rhmg.StudioDiary.InternalWeb/Modules/AvailabilityModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Nancy;
+using Raven.Client;
 using rhmg.StudioDiary.InternalWeb.ViewModels;
 using rhmg.StudioDiary.Raven;
 
@@ -12,32 +13,26 @@ namespace rhmg.StudioDiary.InternalWeb.Modules
         {
             using (var session = store.GetSession())
             {
-                Get["/"] = parameters =>
-                               {
-                                   var thisWeek = DiaryManager.WeekToAViewFor(DateTime.Now, session);
-                                   return View[new AvailabilityWeekModel
-                                   {
-                                       PreviousWeekMonday = thisWeek.MondayDate.AddDays(-7),
-                                       NextWeekMonday = thisWeek.MondayDate.AddDays(7),
-                                       ThisWeek = thisWeek,
-                                       Rooms = Room.All(session),
-                                       Products = Product.All(session)
-                                   }];
-                               };
+                Get["/"] = parameters => GetDiaryForWeek(session, DateTime.Now.Date);
                 Get[@"/(?<day>[\d]{1,2})/(?<month>[\d]{1,2})/(?<year>[\d]{1,4})/"] = parameters =>
                                                    {
                                                        var date = new DateTime(parameters.year, parameters.month, parameters.day);
-                                                       var thisWeek = DiaryManager.WeekToAViewFor(date, session);
-                                                       return View[new AvailabilityWeekModel
-                                                       {
-                                                           PreviousWeekMonday = thisWeek.MondayDate.AddDays(-7),
-                                                           NextWeekMonday = thisWeek.MondayDate.AddDays(7),
-                                                           ThisWeek = thisWeek,
-                                                           Rooms = Room.All(session),
-                                                           Products = Product.All(session)
-                                                       }];
+                                                       return GetDiaryForWeek(session, date);
                                                    };
             }
+        }
+
+        dynamic GetDiaryForWeek(IDocumentSession session, DateTime date)
+        {
+            var thisWeek = DiaryManager.WeekToAViewFor(date, session);
+            return View[new AvailabilityWeekModel
+                            {
+                                PreviousWeekMonday = thisWeek.MondayDate.AddDays(-7),
+                                NextWeekMonday = thisWeek.MondayDate.AddDays(7),
+                                ThisWeek = thisWeek,
+                                Rooms = Room.All(session),
+                                Products = Product.All(session)
+                            }];
         }
     }
 }
